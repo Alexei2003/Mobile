2758bd3e21d6c6c3d9e46c30e284d49635755ccc
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2024-04-23 17:33:58 +0300

    fix

---
+++ Swift/AzurLane/AzurLaneTests/AzurLaneTests.swift
@@ -0,0 +1,34 @@
+//
+//  AzurLaneTests.swift
+//  AzurLaneTests
+//
+//  Created by Prinz Eugen on 21.03.2024.
+//  Copyright © 2024 Prinz Eugen. All rights reserved.
+//
+
+import XCTest
+@testable import AzurLane
+
+class AzurLaneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
